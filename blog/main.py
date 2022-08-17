3d487d1f9260d95d6ce9a2e7cfd1f6aa8151a834
Syyad Khan <syykhan@deloitte.com>
2022-08-17 14:55:03 -0400

    full crud

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 from typing import Optional, Union
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .db import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -17,10 +17,43 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 async def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_blog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return "Done"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"not found with id: {id} not found")
+    blog.update(
+        request.dict()
+    )  # 2:05 https://stackoverflow.com/questions/67023417/attributeerror-blog-object-has-no-attribute-items-fastapi
+    db.commit()
+    return "updated"
+
+
+@app.get("/blog", status_code=status.HTTP_202_ACCEPTED)
+async def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}", status_code=200)
+async def get_blog(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} was not found.")
+    return blog
